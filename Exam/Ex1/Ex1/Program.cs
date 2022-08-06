24e8915c56c48ed5541ff2f50decde3014b169bf
tranthingocanh22 <103102505+tranthingocanh22@users.noreply.github.com>
2022-08-06 18:46:09 +0700

    Exam

---
+++ Exam/Ex1/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Cylinder
+{
+    static void Main(String[] args)
+    {
+
+        double Radius;
+        double Height;
+        double BaseArea;
+        double LateralArea;
+        double TotalArea;
+        double Volume;
+        Console.WriteLine("Enter the dimenstions of the cylinder");
+        Console.Write("Radius : ");
+        Radius = double.Parse(Console.ReadLine());
+        Console.Write("Height : ");
+        Height = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nCylinder Characteristics");
+        Console.WriteLine("Radius: " + Radius + ", Height: " + Height);
+        
+        BaseArea = Radius * Radius * Math.PI;
+        LateralArea = 2 * Math.PI * Radius * Height;
+        TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+        Volume = Math.PI * Radius * Radius * Height;
+        Console.WriteLine("Base: " + BaseArea + " | Latera: " + LateralArea + " | Total: " + TotalArea + " | Volume: " + Volume);
+    }
+}
---
+++ Exam/Ex2/Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿class Base
+{
+    string name;
+    double weight;
+
+    public void SetMe(string name, double weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+    public void Show()
+    {
+        Console.WriteLine("Name: " + name + ", Weight: " + weight);
+    }
+}
+
+class Lion : Base
+{
+    public Lion(string name, double weight)
+    {
+        SetMe(name, weight);
+        Show();
+    }
+}
+
+class Tiger : Base
+{
+    public Tiger(string name, double weight)
+    {
+        SetMe(name, weight);
+        Show();
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Tiger objTiger = new Tiger("Tiger", 100);
+
+        Lion objLion = new Lion("Lion", 200);
+    }
+}
